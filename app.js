1493474c956eb39617adc2bbb8f9669c8b1881ad
Hussein <tejanahmedhu@gmail.com>
2021-03-08 03:24:44 +0000

    test

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+var env = require('dotenv').config();
 var faker = require('faker');
+var mysql = require('mysql');
 
 
 // console.log(faker.internet.email());
@@ -17,3 +19,21 @@ generateAddress();
 generateAddress();
 
 generateAddress();
+
+var connection = mysql.createConnection({
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,  
+    database : process.env.DB_DATABASE,  
+    password : process.env.MYSQL_PW  
+  });
+
+var qry = 'SELECT CURDATE() AS date, CURTIME() AS time, NOW() AS now';
+
+connection.query(qry, function(error, res, fields){
+    if(error) throw error;
+    console.log(res[0].date.toString());
+    console.log(res[0].time.toString());
+    console.log(res[0].now.toString());
+});
+
+connection.end();
