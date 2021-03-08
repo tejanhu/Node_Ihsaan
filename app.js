45c2a5f5c31843e10d79e70ec87c1d59ece1821b
Hussein <tejanahmedhu@gmail.com>
2021-03-08 02:39:21 +0000

    second commit using faker library

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+var faker = require('faker');
+
+
+// console.log(faker.internet.email());
+
+// console.log(faker.date.past());
+
+function generateAddress(){
+    console.log(faker.address.streetAddress())
+    console.log(faker.address.city())
+    console.log(faker.address.state())
+}
+
+
+generateAddress();
+
+generateAddress();
+
+generateAddress();
