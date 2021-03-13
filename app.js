3aca304d3544f90ea4b99908155de6188007887f
Hussein <tejanahmedhu@gmail.com>
2021-03-13 03:46:46 +0000

    new commits

--- app.js
+++ app.js
@@ -2,24 +2,6 @@ var env = require('dotenv').config();
 var faker = require('faker');
 var mysql = require('mysql');
 
-
-// console.log(faker.internet.email());
-
-// console.log(faker.date.past());
-
-function generateAddress(){
-    console.log(faker.address.streetAddress())
-    console.log(faker.address.city())
-    console.log(faker.address.state())
-}
-
-
-generateAddress();
-
-generateAddress();
-
-generateAddress();
-
 var connection = mysql.createConnection({
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,  
@@ -27,13 +9,49 @@ var connection = mysql.createConnection({
     password : process.env.MYSQL_PW  
   });
 
-var qry = 'SELECT CURDATE() AS date, CURTIME() AS time, NOW() AS now';
+//SELECT DATA
+// var qry = 'SELECT COUNT(*) AS total FROM users';
 
-connection.query(qry, function(error, res, fields){
-    if(error) throw error;
-    console.log(res[0].date.toString());
-    console.log(res[0].time.toString());
-    console.log(res[0].now.toString());
-});
+// connection.query(qry, function(error, res, fields){
+//     if(error) throw error;
+//     console.log(res[0].total);
+// });
+
+//INSERT DATA
+// var qry = 'INSERT INTO users (email) VALUES("tyler_the_mouse@yahoo.com")';
+
+// connection.query(qry, function(error, res, fields){
+//     if(error) throw error;
+//     console.log(res);
+// }); 
+
+
+//INSERT DATA II
+// var person = {email: faker.internet.email(),
+//               created_at: faker.date.past()
+// };
+
+// var end_res = connection.query('INSERT INTO users SET ?', person, function(error, res){
+//     if(error) throw error;
+//     console.log(res);
+// });
+
+
+// INSERT LOADS OF DATA ===============================
+
+var data =[];
+for(var i =0; i<500; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+
+connection.query(q, [data], function(err, res){
+    console.log(err);
+    console.log(res);
+})
 
 connection.end();
