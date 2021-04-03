46e62d38827d8f952ea55db0f7df41be7be2dc84
Hussein <tejanahmedhu@gmail.com>
2021-04-03 02:38:56 +0100

    Added functionality to read file

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-var calc = require('./calc.js')
+var fs = require('fs')
 
-res = calc.add(2,2)
-
-res2 = calc.sub(2,2)
-
-res3 = calc.div(2,2)
-
-res4 = calc.mod(2,2)
-
-
-console.log("The addition result is: " + res + "\n")
-
-console.log("The subtraction result is: " + res2 + "\n")
-
-console.log("The division result is: " + res3 + "\n")
-
-console.log("The modulo divisor result is: " + res4 + "\n")
+fs.readFile('calc.js','utf-8', function(err, data){
+    console.log(data)
+})
