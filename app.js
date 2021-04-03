924c20732ef42a1a75f911f724e9d6246c32bc4f
Hussein <tejanahmedhu@gmail.com>
2021-04-03 02:11:17 +0100

    Added http create server

--- app.js
+++ app.js
@@ -1,57 +1,6 @@
-var env = require('dotenv').config();
-var faker = require('faker');
-var mysql = require('mysql');
+var http = require('http');
 
-var connection = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,  
-    database : process.env.DB_DATABASE,  
-    password : process.env.MYSQL_PW  
-  });
-
-//SELECT DATA
-// var qry = 'SELECT COUNT(*) AS total FROM users';
-
-// connection.query(qry, function(error, res, fields){
-//     if(error) throw error;
-//     console.log(res[0].total);
-// });
-
-//INSERT DATA
-// var qry = 'INSERT INTO users (email) VALUES("tyler_the_mouse@yahoo.com")';
-
-// connection.query(qry, function(error, res, fields){
-//     if(error) throw error;
-//     console.log(res);
-// }); 
-
-
-//INSERT DATA II
-// var person = {email: faker.internet.email(),
-//               created_at: faker.date.past()
-// };
-
-// var end_res = connection.query('INSERT INTO users SET ?', person, function(error, res){
-//     if(error) throw error;
-//     console.log(res);
-// });
-
-
-// INSERT LOADS OF DATA ===============================
-
-var data =[];
-for(var i =0; i<500; i++){
-    data.push([
-        faker.internet.email(),
-        faker.date.past()
-    ]);
-}
-
-var q = 'INSERT INTO users (email, created_at) VALUES ?';
-
-connection.query(q, [data], function(err, res){
-    console.log(err);
-    console.log(res);
-})
-
-connection.end();
+http.createServer(function(req,res){
+    res.writeHead("200",{"Content-Type":"text/html"});
+    res.end("Hello World");
+ }).listen(8080)
