cbe8efd63aa02d2903223ab249baec83db283ec3
Hussein <tejanahmedhu@gmail.com>
2021-04-03 02:49:28 +0100

    Added fs -read -write -append -delete functionality

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var fs = require('fs')
 
-fs.appendFile('calc.js','\nconsole.log("just appended this to the file")', function(err){
-    console.log("File appended!")
+// fs.writeFile('calc1.js','console.log("Created new file!")', function(err){
+//     console.log("File created successfully!")
+// })
+
+// fs.readFile('calc1.js','utf-8', function(err,data){
+//     console.log(data)
+// })
+
+// fs.appendFile('calc1.js','\nconsole.log("Appended file successfully!")', function(err){
+//     console.log("File appended successfully!")
+// })
+
+fs.unlink("calc1.js",function(error){
+    console.log("File no longer exists")
 })
