1d1f7fec195946d49a0f2008d24fe815750a99ff
Hussein <tejanahmedhu@gmail.com>
2021-04-03 02:27:27 +0100

    The different ways to export functions in node

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-var http = require('http');
+var calc = require('./calc.js')
 
-http.createServer(function(req,res){
-    res.writeHead("200",{"Content-Type":"text/html"});
-    res.end("Hello World");
- }).listen(8080)
+res = calc.add(2,2)
+
+res2 = calc.sub(2,2)
+
+res3 = calc.div(2,2)
+
+res4 = calc.mod(2,2)
+
+
+console.log("The addition result is: " + res + "\n")
+
+console.log("The subtraction result is: " + res2 + "\n")
+
+console.log("The division result is: " + res3 + "\n")
+
+console.log("The modulo divisor result is: " + res4 + "\n")
---
+++ calc.js
@@ -0,0 +1,20 @@
+function add(n,m){
+return n + m
+}
+
+function sub(n,m){
+    return n - m
+ }
+
+function mod(n,m){
+    return n % m
+ }
+
+ exports.div = function div(n,m){
+    return n / m
+ }
+
+
+exports.add = add
+exports.sub = sub
+module.exports.mod = mod
